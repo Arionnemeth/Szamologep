1e28947219cdef41a7a15ef448cb02c165bb76ba
Arion <Arion@LENOVO-ARION>
2026-09-17 18:28:28 +0200

    Szamologep mechanika befejezve

--- Szamologep/MainWindow.xaml.cs
+++ Szamologep/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -42,8 +43,8 @@ namespace Szamologep
                 for (int j = 0; j < 4; j++) //oszlopok
                 {
                     string label = feliratok[i, j];
-                    Button btn = new Button 
-                    { 
+                    Button btn = new Button
+                    {
                         Content = label,
                         FontSize = 20,
                         FontWeight = FontWeights.Bold,
@@ -61,24 +62,84 @@ namespace Szamologep
                     else
                     {
                         btn.Background = Brushes.DodgerBlue;
-                        btn.Foreground= Brushes.White;
+                        btn.Foreground = Brushes.White;
                     }
 
                     btn.Click += Butto_Click;
-
                     Grid.SetRow(btn, i);
                     Grid.SetColumn(btn, j);
-
                     ButtonGrid.Children.Add(btn);
                 }
             }
         }
 
+        private double elsoSzam = 0;
+        private string muvelet = "";
+        private bool ujSzam = true; 
+
         private void Butto_Click(object sender, RoutedEventArgs e)
         {
-          Button button = sender as Button;
+            Button button = sender as Button;
             string felirat = button.Content.ToString();
-            tb_kijelzo.Text = felirat;
+
+            if(felirat == "C")
+            {
+                tb_kijelzo.Text = "0";
+                elsoSzam = 0;
+                muvelet = "";
+                ujSzam = true;
+            }
+
+            else if (felirat == "+" || felirat == "-" || felirat == "*" || felirat == "/")
+            {
+                elsoSzam = Convert.ToDouble(tb_kijelzo.Text);
+                muvelet = felirat;
+                ujSzam = true;
+            }
+
+            else if (felirat == "=")
+            {
+                double masodikSzam = Convert.ToDouble(tb_kijelzo.Text);
+                double eredmeny = 0;
+
+                switch (muvelet)
+                {
+                    case "+":
+                        eredmeny = elsoSzam + masodikSzam;
+                        break;
+                    case "-":
+                        eredmeny = elsoSzam - masodikSzam;
+                        break;
+                    case "*":
+                        eredmeny = elsoSzam * masodikSzam;
+                        break;
+                    case "/":
+                        eredmeny = elsoSzam / masodikSzam;
+                        break;
+                }
+
+                tb_kijelzo.Text = eredmeny.ToString();
+                ujSzam = true;
+            }
+
+            else
+            {
+                if(tb_kijelzo.Text == "0" || ujSzam)
+                {
+                    tb_kijelzo.Text = felirat;
+                    ujSzam = false;
+                }
+                else
+                {
+                    tb_kijelzo.Text += felirat;
+                }
         }
     }
-}
+        }
+
+       
+                
+            
+        
+
+    }
